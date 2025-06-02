2d3a0c78548dbe99cccc4a5415415b263b07efdf
Mugdho <muhammadmugdho@gmail.com>
2025-06-03 05:05:11 +0600

    Added the sounds for each individual buttons; updated the Javascript to catch the exact button pressed by the user in sequences; and added the animation to show the visual representation through calling the function in the console log

--- game.js
+++ game.js
@@ -1,1 +1,33 @@
-alert("hello");
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var UserClickedPattern = [];
+
+$(".btn").click(function() {
+        var userChosenColor = $(this).attr("id");
+        console.log("User clicked: " + userChosenColor);
+        UserClickedPattern.push(userChosenColor);
+        console.log("User's pattern: " + UserClickedPattern.join(", "));        
+    });
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColors[randomNumber];
+    
+    gamePattern.push(randomChosenColor);
+    
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    
+    var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+    audio.play();
+
+
+
+    console.log("New sequence: " + gamePattern.join(", "))
+}
+
+
+
+
+
+
+
